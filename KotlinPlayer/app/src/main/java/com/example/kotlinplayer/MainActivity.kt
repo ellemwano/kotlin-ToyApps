be2e9125df9396903b45f6a5243ec7b45a13b282
ellemwano <ellemwano@users.noreply.github.com>
2019-05-12 15:17:47 +0200

    Intro to classes, properties and interfaces

--- KotlinPlayer/app/src/main/java/com/example/kotlinplayer/MainActivity.kt
+++ KotlinPlayer/app/src/main/java/com/example/kotlinplayer/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.kotlinplayer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), Logger {
+
+//   override val tag: String = MainActivity::class.java.simpleName
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,9 +22,25 @@ class MainActivity : AppCompatActivity() {
         button.setOnClickListener {
             toast("Hello ${editText.text}")  // String template
         }
+
+        // Debug log (using Logger interface)
+        d("Hello")
     }
 
     private fun toast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
+}
+
+
+/*
+  Interface for logs
+ */
+interface  Logger {
+
+    val tag: String
+        get() = javaClass.simpleName
+
+    fun d(message: String) = Log.d(tag, message)
+
 }
