ca507cf0dea7103fba6c54db8f24a2fe4ace8d7e
ellemwano <ellemwano@users.noreply.github.com>
2019-05-14 21:26:30 +0200

    Collections - Added filtering menu

--- KotlinPlayer/app/src/main/java/com/example/kotlinplayer/MainActivity.kt
+++ KotlinPlayer/app/src/main/java/com/example/kotlinplayer/MainActivity.kt
@@ -2,19 +2,38 @@ package com.example.kotlinplayer
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.RecyclerView
+import android.view.Menu
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
-    val recyclerView by lazy { findViewById<RecyclerView>(R.id.recycler) }
+    val adapter = MediaAdapter(getMedia()) { (title) -> toast(title) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Showing a toast with title on click (extension function for toast)
-        recyclerView.adapter = MediaAdapter(getMedia()) { (title) -> toast(title) }
+        recycler.adapter = adapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // let - Calls the specified function block with this value as its argument and returns its result.
+        adapter.items = getMedia().let {media ->
+            when (item.itemId) {
+                R.id.filter_all -> media
+                R.id.filter_photos -> media.filter { it.type == MediaItem.Type.PHOTO }
+                R.id.filter_videos -> media.filter { it.type == MediaItem.Type.VIDEO }
+                else -> emptyList()
+            }
+        }
+        return true
     }
 }
 
