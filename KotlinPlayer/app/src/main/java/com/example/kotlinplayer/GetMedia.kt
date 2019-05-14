18c1098712a2f1cfcb8787ce92a33e8d5a56f237
ellemwano <ellemwano@users.noreply.github.com>
2019-05-14 21:56:50 +0200

    Ranges - Generated MediaItem list using a range

--- KotlinPlayer/app/src/main/java/com/example/kotlinplayer/GetMedia.kt
+++ KotlinPlayer/app/src/main/java/com/example/kotlinplayer/GetMedia.kt
@@ -1,15 +1,11 @@
 package com.example.kotlinplayer
 
 private val thumbBase = "http://lorempixel.com/400/400/cats/"
-fun getMedia() = listOf(
-    MediaItem("Title 1", "${thumbBase}1", MediaItem.Type.PHOTO),
-    MediaItem("Title 2", "${thumbBase}2", MediaItem.Type.PHOTO),
-    MediaItem("Title 3", "${thumbBase}3", MediaItem.Type.VIDEO),
-    MediaItem("Title 4", "${thumbBase}4", MediaItem.Type.PHOTO),
-    MediaItem("Title 5", "${thumbBase}5", MediaItem.Type.PHOTO),
-    MediaItem("Title 6", "${thumbBase}6", MediaItem.Type.VIDEO),
-    MediaItem("Title 7", "${thumbBase}7", MediaItem.Type.VIDEO),
-    MediaItem("Title 8", "${thumbBase}8", MediaItem.Type.PHOTO),
-    MediaItem("Title 9", "${thumbBase}9", MediaItem.Type.PHOTO),
-    MediaItem("Title 10", "${thumbBase}10", MediaItem.Type.VIDEO)
-)
+
+// map - Returns a list containing the results of applying the given transform function to each element
+// in the original collection.
+fun getMedia() = (1..10).map {
+    MediaItem("Title $it",
+        "$thumbBase$it",
+        if (it % 3 == 0) MediaItem.Type.VIDEO else MediaItem.Type.PHOTO)
+}
